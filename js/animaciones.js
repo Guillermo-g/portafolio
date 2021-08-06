81aaea97ba64d6498bb9b1e799f0b1f965c1c983
Guillermo-g <88546302+Guillermo-g@users.noreply.github.com>
2021-08-06 13:32:00 -0300

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,23 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });*/
+  document.addEventListener('DOMContentLoaded', function() {
+    var options = {};
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+  
